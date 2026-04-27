ce0022cbb1317a1b8d472fe5781d622e7388bd1f
oleg-sfn <olegmaster1005@gmail.com>
2026-04-27 21:10:42 +0300

    Added grafana main dashboard

--- services/api-gateway/internal/middleware/metrics.go
+++ services/api-gateway/internal/middleware/metrics.go
@@ -21,7 +21,7 @@ var (
 		prometheus.HistogramOpts{
 			Name:    "http_request_duration_seconds",
 			Help:    "HTTP request duration in seconds",
-			Buckets: []float64{0.01, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 30},
+			Buckets: []float64{0.01, 0.05, 0.075, 0.1, 0.15, 0.2, 0.25, 0.35, 0.5, 0.75, 1, 1.5, 2.5, 5, 10, 30},
 		},
 		[]string{"method", "path"},
 	)
